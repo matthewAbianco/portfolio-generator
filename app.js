c8cb3fcd85e291b4e4fdd27f0cb07ceec7cbcab0
Matt Bianco <matthewbianco@protonmail.com>
2021-02-27 17:57:16 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+
+// const [name, github] = profileDataArgs;
+
+//   fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+console.log(inquirer);
